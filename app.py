725779811b6e81207b09aa990eeb4f840dd157d2
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2025-06-03 11:47:01 -0300

    App is ready for deploy

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 import zipfile
 from io import BytesIO
+import locale
 
 st.set_page_config(page_title="Gerador de Certificados", layout="centered")
 
@@ -12,7 +13,7 @@ st.title("Gerador de Certificados")
 
 st.subheader("📄 Modelo de certificado")
 
-template_options = ["modelo1", "modelo1", "modelo1"]
+template_options = ["evento_3instituicoes", "modelo1", "modelo1"]
 template = st.selectbox(
     "Selecione o modelo de certificado:",
     template_options,
@@ -39,12 +40,20 @@ if planilha:
 
 font_file = "fonts/Vidaloka-Regular.ttf"
 
+event_name = st.text_input("Nome do Evento", value="Evento1")
+hour = st.text_input("Carga horária (ex.: 8 horas)", value="0 horas")
+locale.setlocale(locale.LC_TIME, "pt_BR.UTF-8")
+date = st.date_input("Insira a data do evento", value="today", format="DD/MM/YYYY").strftime('%d de %B de %Y')
+
 if template and planilha and font_file:
     with st.expander("Gerar certificados"):
         df = pd.read_excel(planilha)
         df = df.drop_duplicates()
         img_template = Image.open(template)
-        font = ImageFont.truetype(font_file, size=85)
+        font_name = ImageFont.truetype(font_file, size=85)
+        font_event = ImageFont.truetype(font_file, size=55)
+        font_hour = ImageFont.truetype(font_file, size=45)
+        font_date = ImageFont.truetype(font_file, size=35)
 
         certificates = []
         preview_image = None
@@ -53,11 +62,36 @@ if template and planilha and font_file:
             nome = row["nome"]
             novo_cert = img_template.copy()
             draw = ImageDraw.Draw(novo_cert)
-            bbox = draw.textbbox((0, 0), nome, font=font)
+            # Puts the name on the template
+            bbox = draw.textbbox((0, 0), nome, font=font_name)
             w = bbox[2] - bbox[0]
             h = bbox[3] - bbox[1]
-            pos = ((novo_cert.width - w) // 2, 468)
-            draw.text(pos, nome, font=font, fill="black")
+            pos = ((novo_cert.width - w) // 2, 424)
+            draw.text(pos, nome, font=font_name, fill="black")
+            
+            # Puts the event on the template
+            bbox = draw.textbbox((0, 0), event_name, font=font_event)
+            w = bbox[2] - bbox[0]
+            h = bbox[3] - bbox[1]
+            pos = ((novo_cert.width - w) // 2, 630)
+            draw.text(pos, event_name, font=font_event, fill="black")
+            
+            # Puts the hours on the template
+            bbox = draw.textbbox((0, 0), hour, font=font_hour)
+            w = bbox[2] - bbox[0]
+            h = bbox[3] - bbox[1]
+            pos = ((novo_cert.width - w) // 2, 769)
+            draw.text(pos, hour, font=font_hour, fill="black")
+            
+            # Puts the date on the template
+            full_date = "Niterói, " + date
+            bbox = draw.textbbox((0, 0), full_date, font=font_date)
+            w = bbox[2] - bbox[0]
+            h = bbox[3] - bbox[1]
+            pos = ((novo_cert.width - w) // 2, 898)
+            draw.text(pos, full_date, font=font_date, fill="black")
+            
+            
             certificates.append(novo_cert)
 
             if idx == 0:  # Save the first certificate for preview
