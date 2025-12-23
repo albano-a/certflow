e02aabe55dfcaa236102e419865e47ee2a0d4b8b
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2025-12-22 22:39:51 -0300

    Update draw_bounding_box import path in app.py
    Changed the import of draw_bounding_box to use src.scripts.functions, reflecting a refactor or reorganization of the codebase.

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
 
-from src import draw_bounding_box
+from src.scripts.functions import draw_bounding_box
 
 st.set_page_config(
     page_title="Gerador de Certificados", layout="centered", page_icon="🦈"
